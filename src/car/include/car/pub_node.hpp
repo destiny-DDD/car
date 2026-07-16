8f8d19aa197e63cbc73f6da140025381f2502c90
destiny <2303306754@qq.com>
2026-07-17 02:15:12 +0800

    1.5

--- src/car/include/car/pub_node.hpp
+++ src/car/include/car/pub_node.hpp
@@ -1,20 +1,20 @@
 #ifndef __PUB_NODE_HPP_
 #define __PUB_NODE_HPP_
 
+#include "SharedTopic.hpp"
+#include "SharedTopicClient.hpp"
 #include <rclcpp/rclcpp.hpp>
 
-namespace car
-{
+namespace car {
 
-class CarPublisher:public rclcpp::Node
-{
+class CarPublisher : public rclcpp::Node {
 public:
-    explicit CarPublisher(const std::string& name);
-    void test();
-private:
+  explicit CarPublisher(const std::string &name);
+  void test();
 
+private:
 };
 
-}
+} // namespace car
 
 #endif
