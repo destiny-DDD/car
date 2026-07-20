1516f90537c6792d654fdd85385af0a65d318da8
destiny <2303306754@qq.com>
2026-07-21 02:36:30 +0800

    增加README.md，加入imu模块

--- src/car/launch/car_launch.py
+++ src/car/launch/car_launch.py
@@ -4,9 +4,17 @@ import launch_ros
 from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('car'),
+        'config',
+        'params.yaml',
+    ),
+
     action_car=launch_ros.actions.Node(
         package='car',
         executable='car',
+        parameters=[config],
+        output='screen',
     )
 
     return launch.LaunchDescription([
--- src/heart/launch/launch.py
+++ src/heart/launch/launch.py
@@ -8,12 +8,16 @@ def generate_launch_description():
     pkg_dir_heart = get_package_share_directory("heart")
     pkg_dir_car = get_package_share_directory("car")
     pkg_dir_MID360=get_package_share_directory("livox_ros_driver2")
+    pkg_dir_imu=get_package_share_directory("yesense_std_ros2")
 
     # 默认 xacro 文件路径
     xacro_file = os.path.join(pkg_dir_heart, "urdf", "car.urdf.xacro")
     default_rviz_config_path=os.path.join(pkg_dir_heart,"config","rviz.rviz")
     MID360_launch = os.path.join(
         pkg_dir_MID360,"launch_ROS2","msg_MID360_launch.py"
+    )
+    imu_launch = os.path.join(
+        pkg_dir_imu,"launch","yesense_node.launch.py"
     )
     car_launch = os.path.join(
         pkg_dir_car,"launch","car_launch.py"
@@ -62,6 +66,12 @@ def generate_launch_description():
         )
     )
 
+    action_imu_launch = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            imu_launch
+        )
+    )
+
     return launch.LaunchDescription([
         action_car_launch,
         action_declare_arg_mode_path,
@@ -70,4 +80,5 @@ def generate_launch_description():
         action_joint_state_publisher,
         action_rviz_node,
         action_MID360_launch,
+        action_imu_launch,
     ])
